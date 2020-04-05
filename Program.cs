c753dcbaf2b32e9ed7e4748bef974669b5798917
Denis Rafi <62852838+DenisError404@users.noreply.github.com>
2020-04-05 18:13:51 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your current weight: ");
+            double weightEarth = double.Parse(Console.ReadLine());
+
+            double weightMoon = weightEarth * 0.17;
+            Console.WriteLine("Your weight {0} kg. on the Moon will be: {1} kg.", weightEarth, weightMoon);
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
